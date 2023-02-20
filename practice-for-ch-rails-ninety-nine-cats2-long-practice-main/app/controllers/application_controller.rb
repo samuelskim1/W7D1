dcd124c82b6b889a8ec2538ee0a326da472ae1e2
Kelly Zhou <yinglinzhou0@gmail.com>
2023-02-20 17:25:08 -0500

    header added

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-helper_method :current_user
+helper_method :current_user, :logged_in?
 
     def login(user)
         session[:session_token] = user.reset_session_token!
@@ -9,5 +9,14 @@ helper_method :current_user
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+    end
 
 end
